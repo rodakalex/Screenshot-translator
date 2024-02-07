155d9e3f88d02c8c0384fd6062c0e196b7060c75
Your Name <you@example.com>
2024-02-07 17:33:45 +0300

    Код рефакторинг

--- mangaTranslator/make_screen.py
+++ mangaTranslator/make_screen.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from PIL import ImageGrab
 import pytesseract
 from PIL import Image
-import os
 
 
 def get_text_from_screen() -> str:
@@ -17,7 +16,6 @@ def get_formatter_text():
 class ScreenshotApplication(tk.Tk):
     def __init__(self):
         super().__init__()
-
         self.text = None
         self.y = None
         self.x = None
@@ -25,40 +23,22 @@ class ScreenshotApplication(tk.Tk):
         self.begin_y = None
         self.end_x = None
         self.end_y = None
-
         self.canvas = tk.Canvas(self, cursor="cross")
         self.canvas.bind("<ButtonPress-1>", self.on_button_press)
-        self.canvas.bind("<B1-Motion>", self.on_move_press)
         self.canvas.bind("<ButtonRelease-1>", self.on_button_release)
         self.canvas.pack(fill=tk.BOTH, expand=True)
-
         self.rect = None
-
         self.overrideredirect(True)
-        self.attributes('-transparentcolor', 'white')
-        self.attributes("-alpha", 0.1)
-        self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+0+0")
+        self.wait_visibility(self)
+        self.wm_attributes("-alpha", 0)
+        self.geometry(f"1024x768+0+0")
+
+        # Полный экран
+        # self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+0+0")
 
     def on_button_press(self, event):
         self.begin_x = self.canvas.canvasx(event.x)
         self.begin_y = self.canvas.canvasy(event.y)
-
-    def on_move_press(self, event):
-        cur_x = self.canvas.canvasx(event.x)
-        cur_y = self.canvas.canvasy(event.y)
-
-        w, h = self.canvas.winfo_width(), self.canvas.winfo_height()
-
-        if event.x < 0:
-            cur_x = 0
-        if event.y < 0:
-            cur_y = 0
-        if event.x > w:
-            cur_x = w
-        if event.y > h:
-            cur_y = h
-
-        self.canvas.coords(self.rect, self.begin_x, self.begin_y, cur_x, cur_y)
 
     def on_button_release(self, event):
         self.end_x = self.canvas.canvasx(event.x)
@@ -74,19 +54,13 @@ class ScreenshotApplication(tk.Tk):
         y2 = max(self.begin_y, self.end_y)
 
         img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
-        folder_path = './assets'
-        file_count = sum([1 for _ in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, _))]) + 1
         img.save('./screenshot.png')
-        # img.save(f"./assets/{file_count}.png")
-        self.text = get_formatter_text()
-        # print(self.text)
 
 
 def take_screenshot():
     app = ScreenshotApplication()
     app.mainloop()
     app.destroy()
-    return app.text
 
 
 if __name__ == '__main__':
